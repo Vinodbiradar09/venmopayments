6e865c8457af53e0e2b595cea9cae2d5acacffab
Vinodbiradar09 <vinodjb07@gmail.com>
2025-10-27 16:08:06 +0530

    build workflows

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,16 +1,24 @@
+// import { NextResponse } from "next/server"
+// import  prisma  from "@repo/db/client";
+
 import { NextResponse } from "next/server"
-import { PrismaClient } from "@repo/db/client";
 
-const client = new PrismaClient();
+// export const GET = async () => {
+//     await prisma.user.create({
+//         data: {
+//             email: "asd",
+//             name: "adsads",
+//             number : "7204832222",
+//             password : "hello"
+//         }
+//     })
+//     return NextResponse.json({
+//         message: "hi there"
+//     })
+// }
 
-export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "asd",
-            name: "adsads"
-        }
-    })
+export const GET = async()=>{
     return NextResponse.json({
-        message: "hi there"
-    })
+        message : "hello from the merchant app"
+    },{status : 200})
 }
